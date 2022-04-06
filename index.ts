9210e3e3666c8f28edd71342d74b80f97eb65cfc
felipehw <felipehw@gmail.com>
2022-04-06 17:45:38 -0300

    Backend setup

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const message: String = 'Hello :D';
+
+console.log(message);
