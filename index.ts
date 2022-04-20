29fa4802ef66e9fc49fafe3ae90db3b85194f64a
felipehw <felipehw@gmail.com>
2022-04-20 08:39:13 -0300

    index.js Setup

--- index.ts
+++ index.ts
@@ -3,6 +3,18 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(bodyParser.json({
+    limit: '30mb',
+    //extended: true, // the tutorial contains this, but it is not a valid key
+}));
+app.use(bodyParser.urlencoded({
+    limit: '30mb',
+    extended: true, // true is the default value. See more at <https://www.npmjs.com/package/body-parser>
+}));
+app.use(cors()); // Enable All CORS Requests
+
 const message: String = 'Hello :D';
 
 console.log(message);
