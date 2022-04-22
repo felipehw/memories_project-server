68eb617b209d13550e32d1a97f9585ebf833fe43
felipehw <felipehw@gmail.com>
2022-04-22 09:24:47 -0300

    Connecting to the Database

--- index.ts
+++ index.ts
@@ -3,6 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const MONGODB_USERNAME = 'felipehw';
+const MONGODB_PASSWORD = '389rfhj93';
+const MONGODB_CONNECTION_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.dwls5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(bodyParser.json({
@@ -15,6 +21,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors()); // Enable All CORS Requests
 
-const message: String = 'Hello :D';
-
-console.log(message);
+mongoose.connect(MONGODB_CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`);
+        });
+    })
+    .catch((error) => console.error(error.message || error));
