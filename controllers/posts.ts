1d73a8d66986b6c1ec5242a9c7e21a2fb44c2368
felipehw <felipehw@gmail.com>
2022-04-26 10:23:25 -0300

    Controllers

--- controllers/posts.ts
+++ controllers/posts.ts
@@ -1,7 +1,28 @@
 import express from "express";
 
-const getPosts = (req: express.Request, res: express.Response) => {
-    res.send('<strong>THIS WORKS</strong>');
+import PostMessage from "../models/postMessage";
+
+const getPosts = async (req: express.Request, res: express.Response) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error: any) {
+        res.status(404).json({ message: error.message || error});
+    }
 };
 
-export { getPosts };
+const createPost = async (req: express.Request, res: express.Response) => {
+    const postData = req.body;
+    const newPost = new PostMessage(postData);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error: any) {
+        res.status(409).json({ message: error.message || error});
+    }
+};
+
+export { createPost, getPosts };
--- routes/posts.ts
+++ routes/posts.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { getPosts } from "../controllers/posts";
+import { createPost, getPosts } from "../controllers/posts";
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
